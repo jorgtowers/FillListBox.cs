5d31af494578ee6acaeb2b7091a95e567378b515
Jorge Torres <jorgtowers@gmail.com>
2013-01-23 10:47:48 -0430

    Clase para llenar los ListBox
    Clase preparada para llenar los ListBox, se requiere que la clase
    generica T, implemente la interfaz IDescripcion_Id

---
+++ FillListBox.cs
@@ -0,0 +1,14 @@
+﻿public class NameYourClass
+{
+     private void FillListBox<T>(List<T> listado, ListBox ddl) where T : IDescripcion_Id
+        {
+            T obj = (T)Activator.CreateInstance(listado.GetType().GetGenericArguments().First());
+            obj.Id = -1;
+            obj.Descripcion = "(-- Seleccionar --)";
+            listado.Add(obj);
+            ddl.DataSource = listado.OrderBy(x => x.Descripcion).ToList(); ;
+            ddl.DataTextField = "Descripcion";
+            ddl.DataValueField = "Id";
+            ddl.DataBind();
+        }
+}
---
+++ IDescripcion_Id.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NameYourNameSpace
+{
+    public interface IDescripcion_Id
+    {
+        int Id { get; set; }
+        string Descripcion { get; set; }
+    }
+
+  
+
+
+
+}
